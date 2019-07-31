41bbf28e2ff01dd53427fb6a7fd38f8fbdb0086c
Ian Khor <ikhor@oculo.com.au>
2019-07-31 19:27:29 +1000

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,40 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { type } from './test-utils';
 import App from './App';
 
+const shallowComponent = (props = {}) => shallow(<App {...props} />);
+
 it('renders without crashing', () => {
-  shallow(<App />);
+  shallowComponent();
+});
+
+it('has a form element', () => {
+  expect(
+    shallowComponent()
+      .find('form')
+      .exists()
+  ).toEqual(true);
+});
+
+fdescribe('username length', () => {
+  const errorElement = <div>Username too long</div>;
+
+  describe('less than 5 inclusive', () => {
+    it('does not display error message', () => {
+      const component = shallowComponent();
+      type(component, "input[name='username']", '12345');
+
+      expect(component.contains(errorElement)).toEqual(false);
+    });
+  });
+
+  describe('more than 5', () => {
+    it('displays error message', () => {
+      const component = shallowComponent();
+      type(component, "input[name='username']", '123456');
+
+      expect(component.contains(errorElement)).toEqual(true);
+    });
+  });
 });
