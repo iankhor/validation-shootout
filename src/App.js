21c037d1dcd36aee4c89da0ddd2ef77c65adb9c5
Ian Khor <ikhor@oculo.com.au>
2019-08-01 15:17:47 +1000

    code for confirmaing password

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import useForm from 'react-hook-form';
 import './App.css';
 
 const App = () => {
-  const { register, errors, watch, setValue } = useForm({ mode: 'onBlur' });
+  const { register, errors, watch, setValue, getValues } = useForm({ mode: 'onBlur' });
 
   const username = watch('username', '');
   const usernameRegister = register({ maxLength: { value: 5, message: 'Username too long' } });
   const usernameOnChange = ({ target: { value } }) => setValue(value);
 
   const passwordRegister = register({ required: true });
-  const confirmPasswordRegister = register({ required: true });
+
+  const passwordValidator = ({ psw, confirmPsw }) => psw === confirmPsw || 'Passwords do not match';
+  const confirmPasswordRegister = register({ required: true, validate: () => passwordValidator(getValues()) });
+
+  const errorClassName = errorKey => (errors[errorKey] ? 'error' : '');
 
   return (
     <form className="form">
-      <div className={'cell ' + (errors.username ? 'error' : '')}>
+      <div className={'cell ' + errorClassName('username')}>
         <label>User name:</label>
         <input type="text" name="username" value={username} onChange={usernameOnChange} ref={usernameRegister} />
         {errors.username && <div>{errors.username.message}</div>}
@@ -22,14 +26,15 @@ const App = () => {
 
       <div className="cell" />
 
-      <div className="cell">
+      <div className={'cell ' + errorClassName('confirmPsw')}>
         <label>Password:</label>
         <input type="password" name="psw" ref={passwordRegister} />
       </div>
 
-      <div className="cell">
+      <div className={'cell ' + errorClassName('confirmPsw')}>
         <label>Confirm password:</label>
         <input type="password" name="confirmPsw" ref={confirmPasswordRegister} />
+        {errors.confirmPsw && <div>{errors.confirmPsw.message}</div>}
       </div>
 
       <div className="cell">
