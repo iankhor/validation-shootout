040063134da8fa78b6906cfda4df388b5e09c857
Ian Khor <ikhor@oculo.com.au>
2019-08-01 12:55:35 +1000

    refactor to use setValue by react-hooks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import useForm from 'react-hook-form';
 import './App.css';
 
 const App = () => {
-  const [username, setUsername] = useState('');
+  const { register, errors, watch, setValue } = useForm({ mode: 'onBlur' });
 
-  const { register, errors } = useForm({ mode: 'onBlur' });
-  const usernameValidator = register({ maxLength: { value: 5, message: 'Username too long' } });
+  const username = watch('username', '');
+  const usernameRegister = register({ maxLength: { value: 5, message: 'Username too long' } });
+  const usernameOnChange = ({ target: { value } }) => setValue(value);
+
+  const passwordRegister = register({ required: true });
+  const confirmPasswordRegister = register({ required: true });
 
   return (
     <form className="form">
       <div className={'cell ' + (errors.username ? 'error' : '')}>
         <label>User name:</label>
-        <input type="text" name="username" value={username} onChange={({ target: { value } }) => setUsername(value)} ref={usernameValidator} />
+        <input type="text" name="username" value={username} onChange={usernameOnChange} ref={usernameRegister} />
         {errors.username && <div>{errors.username.message}</div>}
       </div>
 
@@ -20,12 +24,12 @@ const App = () => {
 
       <div className="cell">
         <label>Password:</label>
-        <input type="password" name="psw" />
+        <input type="password" name="psw" ref={passwordRegister} />
       </div>
 
       <div className="cell">
         <label>Confirm password:</label>
-        <input type="password" name="psw" />
+        <input type="password" name="confirmPsw" ref={confirmPasswordRegister} />
       </div>
 
       <div className="cell">
