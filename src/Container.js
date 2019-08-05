21f061c82fd78615036f3ac0c6f289aeca80f9a0
Ian Khor <ikhor@oculo.com.au>
2019-08-06 09:04:04 +1000

    setup Formik in Container

--- src/Container.js
+++ src/Container.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import ReactHookForm from './react-hook-form/Form';
 import ReactFinalForm from './react-final-form/Form';
+import FormikForm from './formik/Form';
 import './Container.css';
 
 const Container = () => {
@@ -8,10 +9,16 @@ const Container = () => {
     <Fragment>
       <h1>react-hook-form</h1>
       <ReactHookForm />
+
       <div className="h-divider" />
 
       <h1>React Final Form</h1>
       <ReactFinalForm />
+
+      <div className="h-divider" />
+
+      <h1>Formik</h1>
+      <FormikForm />
     </Fragment>
   );
 };
