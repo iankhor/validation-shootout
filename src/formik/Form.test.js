c0c2009cef441099b43dc73a0e6473fb0e92625f
Ian Khor <ikhor@oculo.com.au>
2019-08-06 08:54:55 +1000

    setup Formik specs

---
+++ src/formik/Form.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { type } from '../test-utils';
+import { mount, shallow } from 'enzyme';
+
+import Form from './Form';
+
+const mountComponent = (props = {}) => mount(<Form {...props} />);
+
+it('renders without crashing', () => {
+  mountComponent();
+});
+
+it('has a form element', () => {
+  expect(
+    mountComponent()
+      .find('form')
+      .exists()
+  ).toEqual(true);
+});
